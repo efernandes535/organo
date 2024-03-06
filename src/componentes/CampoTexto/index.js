e8b3e79dcb49afa057396616609923773a4ee959
Everton Fernandes Medeiros <caverna276@gmail.com>
2024-03-05 21:35:58 -0300

    interagindo com o usuário

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,14 +2,14 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    const placeholderModificada = `${props.placeholder}...` 
+    const placeholderModificada = `${props.placeholder}...`
 
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input required={props.obrigatorio} placeholder={placeholderModificada} />
         </div>
     )
 }
