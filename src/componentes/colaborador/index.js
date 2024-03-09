b1bbc4f61665630bcccc015e5335f9952309f0fe
Everton Fernandes Medeiros <caverna276@gmail.com>
2024-03-09 19:01:49 -0300

    card de colaborador

---
+++ src/componentes/colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/efernandes535.png' alt='Everton Fernandes' />
+        </div>
+        <div className='rodape'>
+            <h4>Everton Fernandes</h4>
+            <h5>Front-End</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
